1acc47395cdb0926e9bd4671819a7d2b7bb00c36
Hugo Gonzalez <pinelo93@gmail.com>
2017-06-29 11:08:47 -0500

    Adds ssl certificate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def letsencrypt
+  	render plain: 'RkwKcLq_gG43O6bxT13FsoVbHXbc3rXwDq2X8PoOuK0.ESaAZOskl9QArHf80V5TrA1RDOhFoB-mW5g0q-bm18M'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'forms#new'
   resources :forms, only: [:create, :new]
+  get '/.well-known/acme-challenge/:id' => 'application#letsencrypt'
 end
