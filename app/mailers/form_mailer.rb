b40cfd71ce948fda43260f9e2cc2f84bb9e0103f
Hugo Gonzalez <pinelo93@gmail.com>
2017-06-30 16:47:46 -0500

    CC

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,7 +1,7 @@
 class FormMailer < ApplicationMailer
 	def send_form(data, recipients)
 		attachments["#{data[:project_name]}.xlsx"] = File.read("#{data[:project_name]}.xlsx")
-		mail(to: recipients, subject: "Reporte - #{data[:project_name]}")
+		mail(to: recipients[0], cc: recipients[1], subject: "Reporte - #{data[:project_name]}")
 	end
 end
 
